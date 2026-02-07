779301ed45cb2287bd9b36346855c1d750b62d0b
Tobias Rosenkrantz Gundersen <gundersen.tobias@gmail.com>
2026-02-07 15:55:11 +0100

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ val distributionsDir = layout.buildDirectory.dir("distributions")
 val runtimesDir = layout.buildDirectory.dir("runtimes")
 
 group = "com.github.trosenkrantz"
-version = "2.0.0"
+version = "2.1.0"
 
 repositories {
     mavenCentral()
